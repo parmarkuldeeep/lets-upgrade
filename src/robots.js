50c2e442053b6185232aefb1d8188c2ee5f185b5
parmarkuldeeep <81968141+parmarkuldeeep@users.noreply.github.com>
2021-08-21 18:35:44 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'ranjit',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    caption:''
+  },
+  {
+    id: 2,
+    name: '',
+    username: '',
+    email: '',
+    caption:''
+  },
+  {
+    id: 3,
+    name: '',
+    username: '',
+    email: 'Nathan@yesenia.net',
+    caption:''
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+    caption:''
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+    caption:''
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info',
+    caption:''
+  },
+  
